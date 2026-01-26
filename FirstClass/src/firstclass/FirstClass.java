c6d774248248963cbc9c4488187468b7317a606a
Rehana Akter <rehanaa2102@gmail.com>
2026-01-27 01:40:31 +0600

    changed

--- FirstClass/src/firstclass/FirstClass.java
+++ FirstClass/src/firstclass/FirstClass.java
@@ -1,4 +1,3 @@
- 
 package firstclass;
 
 import java.util.Scanner;
@@ -6,17 +5,28 @@ import java.util.Scanner;
 public class FirstClass {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        double circleArea = 0;
+        boolean status = false;
+        int count = 1;
         
-        
-        
-        System.out.print("What Is your Name? ");
-        String name = input.nextLine();
-        
-      
-        
-        System.out.println("Welcome " + name + "!");
-       
+        while (status == false && count <= 3) {
+            System.out.print("Enter Your Radius(Zero Or Negetive Number is Not Acceptable): ");
+            float radius = scan.nextFloat();
+            if (radius <= 0) {
+                status = false;
+                count++;
+            } else {
+                circleArea = Math.PI * Math.pow(radius, 2);
+                break;
+            }
+        }
+        if (circleArea > 0) {
+            System.out.println("Your Result is: " + circleArea);
+        } else {
+            System.out.println("Your Have Entered Wrong Number 3 Times. Try Again!");
+        }
+
     }
-    
+
 }
