151ae98d8c3fa90c1fe31ed6d55655599ac445c9
Rehana Akter <rehanaa2102@gmail.com>
2026-03-02 16:08:08 +0600

    File Read Write

---
+++ ExamPreparation/src/exampreparation/FileCWR.java
@@ -0,0 +1,38 @@
+package exampreparation;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileCWR {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            File file = new File("rehana.txt");
+            file.createNewFile();
+            System.out.println("File Created: " + file.getName());
+
+            System.out.print("\nEnter text to Write In File: ");
+            PrintWriter writer = new PrintWriter(file);
+            writer.println(scanner.nextLine());
+            System.out.println("Data written successfully.");
+            writer.close();
+
+            System.out.println("\nFile content:");
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                System.out.println(fileReader.nextLine());
+            }
+            fileReader.close();
+
+        } catch (IOException e) {
+            System.out.println("Error occurred.");
+        }
+
+        scanner.close();
+    }
+}
