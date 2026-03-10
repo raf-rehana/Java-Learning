498c4a8db856122a9371a08b7996fbfcc59af742
Rehana Rafiaah <rehanaa2102@gmail.com>
2026-03-10 16:27:49 +0600

    changed

--- Employee/src/employees/Employees.java
+++ Employee/src/employees/Employees.java
@@ -1,17 +1,7 @@
 package employees;
 
-import employee.dao.EmployeeDao;
-import employee.model.Employee;
-import java.sql.SQLException;
-
 public class Employees {
-    public static void main(String[] args) throws SQLException {
-        Employee employee = new Employee("Sajin", "sajin@gmail.com", "019190000", 500000);
-        EmployeeDao employeeDao = new EmployeeDao();
-
-        employeeDao.saveEmployee(employee);
-//        EmployeeDao.updateEmployee(Employee);
-//        EmployeeDao.deleteEmployee(Employee);
-//        EmployeeDao.showAll();
+    public static void main(String[] args) {
+   
     }
 }
