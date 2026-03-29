ebb11216f5065e44d4b929991016d72f8db43417
Rehana Rafiaah <rehanaa2102@gmail.com>
2026-03-29 18:51:40 +0600

    updated

--- ExamPreparation/src/exampreparation/RecursionMethodFactorial.java
+++ ExamPreparation/src/exampreparation/RecursionMethodFactorial.java
@@ -16,7 +16,7 @@ public class RecursionMethodFactorial {
     }
 
     public static long factorial(int n) {
-        if (n == 0) {
+        if (n <= 0) {
             return 1;
         }
         return n * factorial(n - 1);
