2ca519694b776c49c3e4dd862bd7977dcfab167f
Rehana Rafiaah <rehanaa2102@gmail.com>
2026-03-30 15:17:09 +0600

    Exception Handel

--- ExamPreparation/src/exampreparation/exceptions/ExceptionHandel.java
+++ ExamPreparation/src/exampreparation/exceptions/ExceptionHandel.java
@@ -1,29 +1,29 @@
-
 package exampreparation.exceptions;
 
 import java.util.Scanner;
 
 public class ExceptionHandel {
+
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {        
+        try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
                 try {
                     System.out.print("\nEnter numerator: ");
                     if (scanner.hasNext("q")) {
-                        System.out.println("Exiting calculator!");
+                        System.out.println("Exiting...");
                         break;
                     }
-                    int numerator = scanner.nextInt();                    
+                    int numerator = scanner.nextInt();
                     System.out.print("Enter denominator: ");
                     if (scanner.hasNext("q")) {
-                        System.out.println("Exiting calculator. Goodbye!");
+                        System.out.println("Exiting...");
                         break;
                     }
                     int denominator = scanner.nextInt();
-                    
+
                     int result = numerator / denominator;
                     System.out.println("Result: " + numerator + " / " + denominator + " = " + result);
-                    
+
                 } catch (ArithmeticException e) {
                     System.out.println("Error: Cannot divide by zero!");
                     scanner.nextLine();
